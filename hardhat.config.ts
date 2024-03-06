528e1bff6be941b3da95fa49cf50181d4cc4233a
chefleo <simon.digiorgio@gmail.com>
2024-03-06 12:22:24 +0100

    Add network and dotenv on hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotEnvConfig } from "dotenv";
+dotEnvConfig();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  defaultNetwork: "localhost",
+  solidity: "0.8.20",
+  networks: {
+    hyperbridgetest: {
+      chainId: 55555,
+      url: "https://hyperbridgetest-55555-1.jsonrpc.testnet-srv2.sagarpc.io",
+      accounts: [PRIVATE_KEY as `0x${string}`], // Private Key of your metamask wallet
+    },
+  },
 };
 
 export default config;
